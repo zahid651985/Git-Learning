0a907c66e468f13ed5126617b733ad8498cd9b72
Zahid Hussain <140758315+zahid651985@users.noreply.github.com>
2023-08-12 15:11:33 +0500

    Add files via upload

---
+++ Class 4/Chpt3.js
@@ -0,0 +1,34 @@
+// alert ("Hello");
+var feet = 110;
+var feet2 = feet + 40;
+console.log (feet2)
+var weight = 130;
+var newweight = weight + 20;
+console.log (newweight)
+var originalNum = 23;
+var newNum = originalNum + 7;
+console.log (newNum)
+var originalNum = 90;
+var originalNum = originalNum + 10;
+console.log (originalNum)
+var originalNum = "23";
+var newNum = originalNum + 7;
+console.log (newNum)
+var originalNum = 23;
+var numToBeAdded = 7;
+var newNum = originalNum + numToBeAdded;
+console.log (numToBeAdded)
+// var 1st_President = "Washington"; (illegal var)
+var firstPresident = "Washington";
+console.log (firstPresident)
+// (legal)
+var prezWhoCame1st = "Washington";
+console.log (firstPresident)
+// (legal)
+// alert(144);
+// console.log (144)
+// (automatically converts it to a string and displays)
+var caseQty = 144;
+alert(caseQty);
+
+
