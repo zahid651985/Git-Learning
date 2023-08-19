dee3ae4f925f2ed3d08c59b9c1e331aae0489dcd
Zahid Hussain <140758315+zahid651985@users.noreply.github.com>
2023-08-19 15:24:26 +0500

    Add files via upload

---
+++ Class 6/Chpt4.js
@@ -0,0 +1,69 @@
+// Chapter # 4 Legal / Illegal
+
+// var name = "Zahid";
+// console.log(name)
+
+// var Name ="Zahid";
+// console.log(name)
+
+// var name ="Zahid";
+// console.log(Name)
+
+// var myname ="Zahid Hussain";
+// console.log(myname) 
+
+// // var myName ="Zahid Hussain";
+// // console.log(myName)
+
+// var MyNameIs ="Ali Khan";
+// console.log(mynameis) illegal
+
+// var mynameis="Ali Khan";
+// console.log(mynameis) legal
+
+// var MyNameIs ="Ali Khan";
+// console.log(MyNameIs)  legal
+
+// var apple = 110;
+// var orange = apple + 40;
+// console.log (apple+orange)  legal
+
+// var 1apple = 20;
+// var 1orange = 10;
+// console.log(1apple+1orange)  illegal
+// var apple1 = 20;
+// var orange1 = 20;
+// console.log(apple1+orange1)  legal
+// var age = 25;
+// var name = "John";
+// console.log(age)  legal
+// var userName = "zahidHussain";
+// console.log(username)     illegal
+// var userName = "zahid Hussain";
+// console.log(userName) legal
+// var pi = 3.14159;
+// var pi = 3.14; 
+// console.log(pi+pi) legal
+// var 1pi = 3.14159;
+// var pi2 = 3.14; 
+// // console.log(1pi+pi2)  illegal
+// var P2_a%88$;
+// console.log(P2) illegal
+ // var P2_a%88$;
+// console.log(P2_a%88$)  illegal
+// var $name= "zahid";
+// console.log($name)  illgal
+// var name$= "Ali Khan"
+// console.log(name$) legal
+// var "zahid hussain"= name$;  illgal
+// console.log(name$)
+// var firstleglaChar ="1";
+// console.log(firstleglaChar) legal
+// var 1 = "firstleglaChar";
+// console.log(1)  illegal
+
+// var name = "9w";
+// console.log(9w) illegal
+
+var name = "9w";
+console.log(name) legal
---
+++ Class 6/Chpt5.js
@@ -0,0 +1,57 @@
+// Chapter # 5 Math Expressions : Familiar Operators
+
+// var popularNumber = 4;
+// // alert (popularNumber);
+// alert(2 +2);
+
+// var  popularName = 5;
+// var secondPopulerName = 10;
+// alert(popularName + secondPopulerName)
+
+// var popularNumber = 12 - 24;
+// alert(popularNumber)
+
+// var popularNumber = 4 - 24;
+// alert(popularNumber)
+
+// var popularNumber = 3 * 12;
+// alert(popularNumber)
+
+// var popularNumber = 10 * 100;
+// alert(popularNumber)
+
+// var popularNumber = 2 / 10;
+// alert(popularNumber)
+
+// var popularNumber = 100 / 10;
+// alert(popularNumber)
+
+// var num = 10;
+// var popularNumber = num + 200;
+// alert(popularNumber)
+
+// var popularNumber = 20;
+// var num = popularNumber + 30;
+// // alert(num)
+
+// var num = 10;
+// var anotherNum = 1;
+// var popularNumber = num + anotherNum;
+// alert(popularNumber)
+
+// var firstNum = 2;
+// var secondNum = 5;
+// var popularNumber = firstNum + secondNum;
+// alert(popularNumber)
+
+// var whatsLeftOver = 10 % 3;
+// alert(whatsLeftOver)
+
+// var remainingAmount = 30 % 7;
+// alert(remainingAmount)
+
+// var whatsLeftOver = 9 % 3;
+// alert(whatsLeftOver)
+
+// var num = 20 % 6;
+// alert(num)
