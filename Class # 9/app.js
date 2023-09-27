bf3f16a32d31e431fd9f25279f84a36ca707ad6d
Zahid Hussain <140758315+zahid651985@users.noreply.github.com>
2023-09-27 08:13:27 +0500

    Add files via upload

---
+++ Class # 9/app.js
@@ -0,0 +1,181 @@
+
+// let firstname = "Zahid";
+// // console.log(firstname) /  uncommit this result is  Zahid
+
+// if(firstname == "Zahid") {
+//     console.log("You are a student")
+// }
+// else if(firstname == "Hussain")
+// console.log("No! Hussain is your last name")
+
+// else {
+//     console.log("Don't know you!")
+// }
+// uncommit this and get true result that your are a student
+
+
+// let firstname = "Husnain";
+
+// if(firstname == "Husnian") {
+//     console.log("You are a your teacher")
+// }
+// else if(firstname == "Zahid")
+// console.log("You are a student")
+
+// else {
+//     console.log("Don't know you!")
+// }
+// uncommit this and get false result Don't known you
+
+
+// let firstname = "Husnain";
+
+// if(firstname == "Husnain") {
+//     console.log("Your teacher's name is Husnain")
+// }
+// else if(firstname == "Zahid") {
+// console.log("You are a student")
+// }
+// // uncommit this and get true result Your teacher's name is Husnain
+
+
+// let firstname = "John";
+
+// if(firstname == "Husnain") {
+//     console.log("Your teacher's name is Husnain")
+// }
+// else if(firstname == "Zahid") {
+// console.log("You are a student")
+// }
+// else {
+//     console.log("Don't know you!")
+// }
+// // uncommit this and get false result Don't know you because Hasnain and Zahid not match with firstname John
+
+
+// let age = 18
+
+// if(age < 10) {
+//     console.log("You can't apply")
+// }
+// else if(age > 10) {
+// console.log("You can apply")
+// }
+// else {
+//     console.log("You are not eligible! ")
+// }
+// // // uncommit this and get true value "You can apply" because age > greater then 10 
+
+
+// let num = 10;
+
+//   if (num > 5) {
+
+//     console.log("num is greater than 5");
+//   }
+
+
+// let num = 10;
+
+//   if (num < 11) {
+
+//     console.log("num is less than 11");
+//   }
+
+
+// let num = 10;
+
+//   if (num > 5 || num < 50) {
+
+//     console.log("num is greater than 5 AND less than 50");
+//   }
+
+//  let x = prompt("Where does the Zahid live?");
+//  if (x === "Karachi") {
+//  alert("Correct!");
+//  }
+
+//  else if (x ===  "karachi") {
+//  alert("Correct");
+//  }
+// else if (x === "Khi") {
+// alert("Correct");
+// }
+
+// else if (x === "khi") {
+//     alert("Correct");
+//     }
+
+
+//  let x = prompt("Where does the Zahid live?");
+//  if (x === "Karachi" || x === "karachi" || x === "Khi" || x === "khi" ) {
+//  alert("Correct!");
+//  }
+// else {
+//     alert("could not understand");
+//     }
+
+// let x = prompt("Where does the Zahid live?");
+// let correctAnswer = "Karachi";
+// if (x === correctAnswer) {
+// alert("Correct!");
+// }
+// else {
+//     alert("Not Correct");
+// }
+
+
+// let city = prompt("Where does the Zahid live?");
+// city = city.toLowerCase()
+
+// if (city == "Karachi" || city == "karachi" || city == "Khi" || city == "khi") {
+// alert("Correct!");
+// }
+
+// let city = prompt("Where does the Zahid live?"); 
+// let correctAnswer = "Karachi";
+// if (city === correctAnswer) {
+// score++;
+// userIQ = "genius";
+// alert("Correct!");
+// }
+
+
+
+// let x = prompt("Where does Zahid live?"); 
+// x = x.toLocaleLowerCase()
+// let score = 0; 
+
+// if (x == "Karachi" || x == "karachi" || x == "Khi" || x == "khi") {
+//   score++;
+//   userIQ = "genius";
+//   alert("Correct!");
+// } 
+// else{
+//   userIQ = "not a genius"; 
+//   alert("Incorrect!");
+// }
+// alert("Your score: " + score);
+// alert("Your IQ: " + userIQ);
+
+
+
+// let pets = 8;
+
+// if(pets === 8){
+//     console.log("correct");
+// }
+
+
+let name = prompt ("what is your name?");
+if(name === "Zahid") {
+    alert("Hi, " +name);
+}
+
+else if ( name === "Hussain") {
+    alert("Hussain is your last name");
+}
+else {
+    alert("wrong entry");
+}
+
